2f3b277146f9676b4e7738236626960f02783a34
Delice Lydia <Delydia84@gmail.com>
2021-01-05 11:55:05 +0200

    Setup devise
    - generated user model
    - generated post model
    - created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :posts, only: [:new, :create, :index] 
+  root to: "home#index"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
