0c687d01cfbcf639ce2fccec3db80ac06b6cb2d8
Delice Lydia <Delydia84@gmail.com>
2021-01-05 15:37:26 +0200

    Add list of the posts to the index page
    - Add signin, signup and signout links to the index page
    - Add author name to the post when a user signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @posts = Post.all
     
   end
 
@@ -12,6 +13,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     @post.save
+    redirect_to root_path
   end
 
 
