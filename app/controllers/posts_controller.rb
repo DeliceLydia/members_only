db2faa486d8bf8d6ff721bc82e5e8975f72bd385
smcommits <sm.anas@outlook.com>
2021-01-06 20:58:22 +0530

    Fix linter error for indendation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_to 
+      redirect_to
     else
       render :new
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validates :body, presence: true 
+  validates :body, presence: true
   belongs_to :user
 end
